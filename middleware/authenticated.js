ac10f87f876c8be68a5af954706381f53b121bb3
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-05 13:35:45 -0500

    Se agregó middleware de autenticación para servicio de prueba

---
+++ middleware/authenticated.js
@@ -0,0 +1,32 @@
+const jwt = require('jwt-simple');
+const moment = require('moment');
+const secret = 'clave_secreta_curso';
+
+exports.ensureAuth = (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(403)
+            .json({
+                message: 'La petición no tiene la cabecera de autenticación'
+            });
+    }
+    var token = req.headers.authorization.replace(/['"]+/g, '');
+    try {
+        var payload = jwt.decode(token, secret);
+        if (payload.exp <= moment().unix()) {
+            return res.status(401)
+                .json({
+                    message: 'Token ha expirado'
+                });
+        }
+
+    } catch (err) {
+        console.log(err);
+        return res.status(404)
+            .json({
+                message: 'Token no válido'
+            });
+    }
+
+    req.user = payload;
+    next();
+}
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const api = express.Router();
 const UserController = require('../controllers/user');
+const md_auth = require('../middleware/authenticated');
 
-api.get('/probandoControlador', UserController.pruebas);
+api.get('/probandoControlador', md_auth.ensureAuth, UserController.pruebas);
 
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
