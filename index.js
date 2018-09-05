2c02e87025804eadb99c5b56c10c9e4bfa86da2d
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-04 23:03:34 -0500

    Se creo el servicio web

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict'
 
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
+var app = require('./app');
+process.env.PORT = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost:27017/musifydb', (err) => {
-    if (err) {
-        throw err;
-    }
+mongoose.connect('mongodb://localhost:27017/musifydb', (err, res) => {
+    if (err) throw err;
+
     console.log('Database connected!');
+
+    app.listen(process.env.PORT, () => {
+        console.log(`API REST Server listening at http://localhost:${process.env.PORT}/`);
+    });
 });
