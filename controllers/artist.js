363c89f15e0b76dd26c06786467858c11a63b016
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-05 16:18:43 -0500

    Creación de servicio de guardado de artista

--- controllers/artist.js
+++ controllers/artist.js
@@ -6,9 +6,41 @@ var Album = require('../models/album');
 var Song = require('../models/song');
 
 function getArtist(req, res) {
+    res.json({
+        message: 'Método getArtist del controlador artist.js'
+    });
+}
+
+function saveArtist(req, res) {
+    var params = req.body;
+    var artist = new Artist();
+    artist.name = params.name;
+    artist.desc = params.desc;
+    artist.img = '';
+
+    artist.save((err, artistSaved) => {
+        if (err) {
+            return res.status(500)
+                .json({
+                    message: 'Error al guardar el artista'
+                });
+        }
+
+        if (!artistSaved) {
+            return res.status(400)
+                .json({
+                    message: 'El artista no ha sido guardado'
+                });
+        }
+
+        res.json({
+            artist: artistSaved
+        });
+    });
 
 }
 
 module.exports = {
-    getArtist
+    getArtist,
+    saveArtist
 }
--- routes/artist.js
+++ routes/artist.js
@@ -6,5 +6,6 @@ const ArtistController = require('../controllers/artist');
 const md_auth = require('../middleware/authenticated');
 
 api.get('/artist', md_auth.ensureAuth, ArtistController.getArtist);
+api.post('/artist', md_auth.ensureAuth, ArtistController.saveArtist);
 
 module.exports = api;
