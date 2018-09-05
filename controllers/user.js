b09fca44906e247471613053bff15b2da9a861be
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-05 11:28:01 -0500

    Se agregó servicio para almacenamiento de usuario

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,75 @@
+const bcrypt = require('bcrypt-nodejs');
+
+const User = require('../models/user');
+
 function pruebas(req, res) {
     res.json({
         message: 'Probando una acción del controlador de usuarios del API Rest con NodeJD y MongoDB'
     });
 }
 
+function saveUser(req, res) {
+    var user = new User();
+    var params = req.body;
+
+    console.log(params);
+
+    user.name = params.name;
+    user.surname = params.surname;
+    user.email = params.email;
+    user.role = 'ROLE_USER'; // ROLE_ADMIN
+    user.img = '';
+
+    if (!params.password) {
+        return res.status(500)
+            .json({
+                message: 'Introduce la contraseña'
+            });
+    }
+
+    // Encriptar contraseña y guardar dato
+    bcrypt.hash(params.password, null, null, (err, hash) => {
+        if (err) {
+            return res.status(500)
+                .json({
+                    message: 'Error en la encriptación de la contraseña'
+                });
+        }
+
+        user.password = hash;
+
+        if (user.name === null ||
+            user.surname === null ||
+            user.email === null) {
+            return res.json({
+                message: 'Datos incompletos'
+            });
+        }
+
+        // Almacenamiento de usuario
+        user.save((err, userStored) => {
+            if (err) {
+                return res.status(500)
+                    .json({
+                        message: 'Error en el almacenamiento del usuario'
+                    });
+            }
+            if (!userStored) {
+                return res.status(400).json({
+                    message: 'No se ha almacenado el usuario'
+                });
+            }
+
+            res.json({
+                user: userStored
+            });
+        });
+
+
+    });
+}
+
 module.exports = {
-    pruebas
+    pruebas,
+    saveUser
 };
--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,6 @@ const UserController = require('../controllers/user');
 
 api.get('/probandoControlador', UserController.pruebas);
 
+api.post('/register', UserController.saveUser);
+
 module.exports = api;
