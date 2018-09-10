31eab9d9ebfdd472c41898417e0c74db50360f40
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-10 12:00:39 -0500

    Creación de componente para edición de usuario

---
+++ client/src/app/components/user-edit.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from "@angular/core";
+import { UserService } from '../services/user.service';
+import { User } from "../models/user";
+
+
+@Component({
+    selector: 'user-edit',
+    templateUrl : '../views/user-edit.html',
+    providers : [UserService]
+})
+
+export class UserEditComponent implements OnInit {
+    public titulo : string;
+    public user : User;
+    public identity;
+    public token;
+
+    constructor(private _userService : UserService) {
+        this.titulo = 'Actualizar Datos';
+        this.identity = this._userService.getIdentity();
+        this.token = this._userService.getToken();
+    }
+
+
+    ngOnInit() {
+        console.log('user-edit.component.js cargado');
+    }
+
+}
