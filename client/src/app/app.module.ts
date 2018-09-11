71bfe90f251863e053aca3ebb875a132c86531d4
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-11 13:33:27 -0500

    Se agregó componente de Artistas

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { UserEditComponent } from './components/user-edit.component';
-import { routing, appRoutingProviders } from './app.routing';
+import { ArtistListComponent } from './components/artist-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserEditComponent
+    UserEditComponent,
+    ArtistListComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/components/artist-list.component.ts
@@ -0,0 +1,37 @@
+import {Router, ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { Artist } from '../models/artist';
+import { GLOBAL } from '../services/global';
+
+@Component({
+    selector: 'artist-list',
+    templateUrl : '../views/artist-list.html',
+    providers: [UserService]
+})
+
+export class ArtistListComponent implements OnInit {
+    public titulo : string;
+    public artists : Array<Artist>;
+    public identity;
+    public token;
+    public url : string;
+
+    constructor(
+        private _route: ActivatedRoute,
+        private _router: Router,
+        private _userService: UserService
+    ) {
+        this.titulo = 'Artistas';
+        this.identity = this._userService.getIdentity();
+        this.token = this._userService.getToken();
+        this.url = GLOBAL.url;
+    }
+
+    ngOnInit() {
+        console.log('artist-list.component.js cargado');
+
+        // Conseguir el listado de artistas
+    }
+
+}
