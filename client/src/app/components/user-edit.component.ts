ea2b3e0ec1daf0fc63763f521eb2f20dec70f4d8
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-11 11:51:38 -0500

    Se agregó funcionalidad para actuailzar la imagen del usuario

--- client/src/app/components/user-edit.component.ts
+++ client/src/app/components/user-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { UserService } from '../services/user.service';
 import { User } from "../models/user";
+import { GLOBAL } from '../services/global';
 
 
 @Component({
@@ -16,6 +17,7 @@ export class UserEditComponent implements OnInit {
     public token;
     public alertMessage;
     public filesToUpload: Array<File>;
+    public url : string;
 
     constructor(private _userService : UserService) {
         this.titulo = 'Actualizar Datos';
@@ -24,6 +26,8 @@ export class UserEditComponent implements OnInit {
         this.identity = this._userService.getIdentity();
         this.token = this._userService.getToken();
         this.user = this.identity;
+        this.url = GLOBAL.url;
+        console.log(this.user);
     }
 
     ngOnInit() {
@@ -39,6 +43,22 @@ export class UserEditComponent implements OnInit {
                     // this.user = response.user;
                     localStorage.setItem('identity', JSON.stringify(this.user));
                     document.getElementById("identity_name").innerHTML = this.user.name;
+                    
+                    if (!this.filesToUpload) {
+                        // Redirección
+                    } else {
+                        let url = `${this.url}upload-image-user/${this.user._id}`;
+                        this.makeFileRequest(url, [], this.filesToUpload)
+                            .then((result : any) => {
+                                this.user.img = result.img;
+                                localStorage.setItem('identity', JSON.stringify(this.user));
+                                
+                                let imageUrl = `${this.url}get-image-user/${this.user.img}`;
+                                document.getElementById('imageLogged')
+                                    .setAttribute('src', imageUrl);
+                            });
+                    }
+
                     this.alertMessage = 'Datos actualizados correctamente';
                 }
             },
@@ -55,6 +75,33 @@ export class UserEditComponent implements OnInit {
     }
 
     fileChangeEvent(fileInput : any) {
-        this.filesToUpload = <Array<File>> fileInput;
+        this.filesToUpload = <Array<File>> fileInput.target.files;
+        console.log(this.filesToUpload);
+    }
+
+    makeFileRequest(url : string, params : Array<string>, files : Array<File>) {
+        let token = this.token;
+        return new Promise((resolve, reject) => {
+            let formData : any = new FormData();
+            let xhr = new XMLHttpRequest();
+
+            for (let i = 0; i < files.length; i++) {
+                formData.append('image', files[i], files[i].name);
+            }
+
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        resolve(JSON.parse(xhr.response));
+                    } else {
+                        reject(xhr.response);
+                    }
+                }
+            }
+
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader('authorization', token);
+            xhr.send(formData);
+        });
     }
 }
--- client/src/app/models/user.ts
+++ client/src/app/models/user.ts
@@ -6,6 +6,6 @@ export class User {
         public email : string,
         public password : string,
         public role : string,
-        public image : string
+        public img : string
     ) {}
 }
