6b0b40e3c196b4f485541beaf2c157c4e5b462c2
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-12 10:36:07 -0500

    Creación de componente y formulario para creación de artista

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -3,11 +3,12 @@ import {Routes, RouterModule} from '@angular/router';
 import { UserEditComponent } from './components/user-edit.component';
 import { ArtistListComponent } from './components/artist-list.component';
 import { HomeComponent } from './components/home.component';
-
+import { ArtistAddComponent } from './components/artist-add.component';
 
 const appRoutes : Routes = [
     {path : '', component : HomeComponent},
     {path : 'artists/:page', component : ArtistListComponent},
+    {path : 'create-artist', component : ArtistAddComponent},
     {path : 'mis-datos', component : UserEditComponent},
     {path : '**', component : HomeComponent},
 ];
---
+++ client/src/app/components/artist-add.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from "../services/user.service";
+import { ActivatedRoute, Router } from '@angular/router';
+import { GLOBAL } from '../services/global';
+import { Artist } from '../models/artist';
+
+@Component({
+    selector : 'artist-add',
+    templateUrl : '../views/artist-add.html',
+    providers : [UserService]
+})
+export class ArtistAddComponent implements OnInit {
+    public titulo : string;
+    public artist : Artist;
+    public identity;
+    public token;
+    public url : string;
+
+    constructor(
+        private _route : ActivatedRoute,
+        private _router : Router,
+        private _userService : UserService
+    ) {
+        this.titulo = 'Crear nuevo artista';
+        this.identity = this._userService.getIdentity();
+        this.token = this._userService.getToken();
+        this.url = GLOBAL.url;
+        this.artist = new Artist('', '', '');
+    }
+
+    ngOnInit(): void {
+        console.log('artist-add.component.ts cargado');
+    }
+
+}
