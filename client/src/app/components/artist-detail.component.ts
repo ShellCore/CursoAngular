79321e8237f8984602ee8b3b14f5a1fdb2b7c95f
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-12 22:14:25 -0500

    Se realizó diseño de la vista de detalle de artista

--- client/src/app/components/artist-detail.component.ts
+++ client/src/app/components/artist-detail.component.ts
@@ -46,6 +46,8 @@ export class ArtistDetailComponent implements OnInit {
                             this._router.navigate(['/']);
                         } else {
                             this.artist = response.artist;
+
+                            // TODO Sacar los albums del artista
                         }
                     },
                     error => {
