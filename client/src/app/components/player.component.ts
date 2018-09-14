086d4b3fb208bdccc718765a5728779ddae67e3a
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-14 17:24:42 -0500

    Estilo a la barra de player

--- client/src/app/components/player.component.ts
+++ client/src/app/components/player.component.ts
@@ -1,11 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { GLOBAL } from '../services/global';
+import { Song } from '../models/song';
 
 @Component({
     selector : 'player',
     template:
         `
-            <h1>Player</h1>
+        <div class="album-image">
+            <span *ngIf="song.album">
+                <img id="playImageAlbum" src="{{url + 'get-image-album/' + song.album.img}}">
+            </span>
+            <span *ngIf="!song.album">
+                <img id="playImageAlbum" src="assets/images/default.jpg">
+            </span>
+        </div>
+
+        <div class="audioFile">
+            <p>Reproduciendo</p>
+            <span id="playSongTitle">
+                {{song.name}}
+            </span>
+            <span id="playSongArtist">
+                <span *ngIf="song.album && song.album.artist">
+                    {{song.album.artist.name}}
+                </span>
+            </span>
+        </div>
+
+        <audio controls id="player">
+            <source id="mp3Source" src="{{url + 'get-file-song/' + song.file}}" type="audio/mpeg">
+            Tu navegador no es compatible
+        </audio>
         `
 })
 export class PlayerComponent implements OnInit {
@@ -16,6 +41,7 @@ export class PlayerComponent implements OnInit {
 
     ) {
         this.url = GLOBAL.url;
+        this.song = new Song(0,'','','','');
     }
     
     ngOnInit(): void {
